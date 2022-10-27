89aff092a9acb93e743bdeff92db2d42c39f4c1c
Tuzbay <t.uzbayyelce@gmail.com>
2022-10-27 22:58:13 +0300

    Finish Project

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController')
 
 const app = express();
 
+// Connect MongoDB
+
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(fileUpload());
+
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+// ROUTES
+app.get('/', photoController.getAllPhotos)
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 
